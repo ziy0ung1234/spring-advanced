a6e381dbfccfe66d198eca59dfee7d1a5bf05590
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2025-12-01 20:07:27 +0900

    refactor: 테스트코드 2-1 예외처리 수정

--- src/test/java/org/example/expert/domain/manager/service/ManagerServiceTest.java
+++ src/test/java/org/example/expert/domain/manager/service/ManagerServiceTest.java
@@ -39,14 +39,14 @@ class ManagerServiceTest {
     private ManagerService managerService;
 
     @Test
-    public void manager_목록_조회_시_Todo가_없다면_NPE_에러를_던진다() {
+    public void failToFindTodos_whenManagerTodoListIsEmpty() {
         // given
         long todoId = 1L;
         given(todoRepository.findById(todoId)).willReturn(Optional.empty());
 
         // when & then
         InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> managerService.getManagers(todoId));
-        assertEquals("Manager not found", exception.getMessage());
+        assertEquals("Todo not found", exception.getMessage());
     }
 
     @Test
